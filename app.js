f491a14d513f44991fc3acbf64ff521a0142528e
milanzagorac <zagoracm@gmail.com>
2020-11-15 16:28:20 -0500

    wip

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 async function searchShows(showSearch) {
     let res = await axios.get(`http://api.tvmaze.com/search/shows?q=${showSearch}`);
-    console.log(res.data);
+    
+    let shows = res.data.map(result => {
+        let show = result.show; 
+        return {
+            id: show.id,
+            name: show.name,
+            summary: show.summary,
+        };
+    });
+    return shows;
 }
 
-async function getEpisodeData(searchParam) {
-    let res = await axios.get(`http://api.tvmaze.com/shows/${searchParam}/episodes`);
-    return res;
-}
+// async function getEpisodeData(searchParam) {
+//     let res = await axios.get(`http://api.tvmaze.com/shows/${searchParam}/episodes`);
+//     return res;
+// }
 

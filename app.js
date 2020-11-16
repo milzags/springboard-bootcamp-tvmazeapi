ae8ba2b1f631d40bbeb69cb3292847720ebb8258
milanzagorac <zagoracm@gmail.com>
2020-11-15 19:56:54 -0500

    updating

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 async function searchShows(showSearch) {
     let res = await axios.get(`http://api.tvmaze.com/search/shows?q=${showSearch}`);
-    
+    console.log(res.data);
+    // map method is used to transform each element in the results array,
+    // create a new array, and return the new array (array of objects). 
     let shows = res.data.map(result => {
-        let show = result.show; 
         return {
-            id: show.id,
-            name: show.name,
-            summary: show.summary,
+            id: result.show.id,
+            name: result.show.name,
+            summary: result.show.summary,
         };
     });
     return shows;
 }
 
-// async function getEpisodeData(searchParam) {
-//     let res = await axios.get(`http://api.tvmaze.com/shows/${searchParam}/episodes`);
-//     return res;
-// }
+const populateShow = (shows) => {
+    let showsList = document.getElementById('shows-list');
+
+}
 

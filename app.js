a988b5fc07e182ce1b3c34cb518875c62d5ab0a3
milanzagorac <zagoracm@gmail.com>
2020-11-22 16:37:52 -0500

    final assignment

--- app.js
+++ app.js
@@ -14,7 +14,64 @@ async function searchShows(showSearch) {
 }
 
 const populateShow = (shows) => {
-    let showsList = document.getElementById('shows-list');
+    // get the location where the shows will be dynamically added
+    let $showsList = $('#shows-list');
+    // clear the container where the shows are stored before we start adding to it
+    $showsList.empty();
 
+    for (let show of shows) {
+        let $item = $(
+          `<div class="col-md-4 col-lg-3 Show" data-show-id="${show.id}">
+             <div class="card" data-show-id="${show.id}">
+               <div class="card-body">
+                 <h4 class="card-title">${show.name}</h4>
+                 <p class="card-text">${show.summary}</p>
+                 <button class="btn btn-primary get-episodes">Episodes</button>
+               </div>
+             </div>  
+           </div>
+          `);
+    
+        $showsList.append($item);
+    }  
 }
+
+async function getEpisodes(showid) {
+    let response = await axios.get(`http://api.tvmaze.com/shows/${showid}/episodes`);
+    // console.log(response);
+
+    let episodes = response.data.map(episode => {
+        return {
+            id : episode.id,
+            name: episode.name,
+            season: episode.season,
+            number: episode.number,
+        }
+    });
+    return episodes;
+};
+
+function populateEpisodes(episodes) {
+    const $episodesList = $('#episodes-list');
+    $episodesList.empty();
+
+    for (let episode of episodes) {
+        let $item = $(
+            `<li>
+                ${episode.name}, ${episode.season}, ${episode.number}
+            </li>`
+        );
+
+        $episodesList.append($item);
+
+    };
+
+    $('#episodes-area').show();
+}
+
+$("#shows-list").on("click", ".get-episodes", async function handleEpisodeClick(evt) {
+    let showId = $(evt.target).closest(".Show").data("show-id");
+    let episodes = await getEpisodes(showId);
+    populateEpisodes(episodes);
+  });
 
